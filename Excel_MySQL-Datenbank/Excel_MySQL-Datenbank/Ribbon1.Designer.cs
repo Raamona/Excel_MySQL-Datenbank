f690309871948c1dfecb4fdb9cd918c423163de2
Raamona <ramona-hauck@web.de>
2016-08-11 17:38:44 +0200

    MySQL-Datenbank in Excel importieren

---
+++ Excel_MySQL-Datenbank/Excel_MySQL-Datenbank/Ribbon1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Excel_MySQL_Datenbank
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">"true", wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls "false".</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Komponenten-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für Designerunterstützung -
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.button2 = this.Factory.CreateRibbonButton();
+            this.button3 = this.Factory.CreateRibbonButton();
+            this.button4 = this.Factory.CreateRibbonButton();
+            this.button5 = this.Factory.CreateRibbonButton();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "Test";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.button1);
+            this.group1.Items.Add(this.button2);
+            this.group1.Items.Add(this.button3);
+            this.group1.Items.Add(this.button4);
+            this.group1.Items.Add(this.button5);
+            this.group1.Label = "Test";
+            this.group1.Name = "group1";
+            // 
+            // button1
+            // 
+            this.button1.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button1.Label = "MySQL";
+            this.button1.Name = "button1";
+            this.button1.OfficeImageId = "HappyFace";
+            this.button1.ShowImage = true;
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button2.Label = "Als CSV speichern";
+            this.button2.Name = "button2";
+            this.button2.OfficeImageId = "HappyFace";
+            this.button2.ShowImage = true;
+            this.button2.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button3.Label = "CSV in Excel schreiben";
+            this.button3.Name = "button3";
+            this.button3.OfficeImageId = "HappyFace";
+            this.button3.ShowImage = true;
+            this.button3.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button4.Label = "Datenbank in MessageBox ";
+            this.button4.Name = "button4";
+            this.button4.OfficeImageId = "HappyFace";
+            this.button4.ShowImage = true;
+            this.button4.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.button5.Label = "MySQL-Datenbank in Excel";
+            this.button5.Name = "button5";
+            this.button5.OfficeImageId = "HappyFace";
+            this.button5.ShowImage = true;
+            this.button5.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button5_Click);
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button2;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button3;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button4;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button5;
+
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ Excel_MySQL-Datenbank/Excel_MySQL-Datenbank/Ribbon1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Office.Tools.Ribbon;
+using MySql.Data.MySqlClient;
+using System.Runtime.InteropServices;
+using System.Reflection;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+using Microsoft.Office.Tools.Excel;
+using System.Data.Odbc;
+using System.Configuration;
+
+namespace Excel_MySQL_Datenbank
+{
+    public partial class Ribbon1
+    {
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            int zeile = 1;
+            string[] separator = { ",", "\r\n", };
+            var ersteZeile = File.ReadLines("C:\\Users\\Ramona\\Desktop\\Praktikum\\MySQL\\auto.csv").First();
+            //var --> definition einer nicht typisierten Variable (konkreter Typ ergibt sich aus dem zugewiesenen Wert)
+            int zähler = ersteZeile.Split(separator, StringSplitOptions.None).Length;
+            string spalte = Char.ConvertFromUtf32(zähler + 64);
+            var reader = new StreamReader(File.OpenRead("C:\\Users\\Ramona\\Desktop\\Praktikum\\MySQL\\auto.csv"));
+            List<string[]> values = new List<string[]>(); //new erstellt Objekt, ruft Konstruktor auf
+
+            Excel.Window window = e.Control.Context; //aktives Fenster wird dargestellt
+            Excel.Worksheet activeWorksheet = ((Excel.Worksheet)window.Application.ActiveSheet);
+            //gibt ein Objekt zurück, das das aktuelle Blatt in der aktiven Arbeitsmappe darstellt 
+            Excel.Range newWorksheet = activeWorksheet.get_Range("A" + zeile, spalte + zeile);
+            //stellt eine Zelle/ein Zellenbereich dar   
+
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                values.Add(line.Split(separator, StringSplitOptions.None)); //erste Zeile wird gesplittet hinzugefügt
+                newWorksheet.Value2 = line.Split(separator, StringSplitOptions.None);
+                zeile++;
+                while (!reader.EndOfStream)
+                {
+                    Excel.Range newWorksheet2 = activeWorksheet.get_Range("A" + zeile, spalte + zeile);
+                    string newline = reader.ReadLine();
+                    values.Add(newline.Split(separator, StringSplitOptions.None));
+                    newWorksheet2.Value2 = newline.Split(separator, StringSplitOptions.None);
+                    zeile++;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, RibbonControlEventArgs e)
+        {
+            try
+            {
+                MySqlConnection mcon = new MySqlConnection("datasource=localhost;port=3306;username=root;password=");
+                //Stellt eine Verbindung zur Datenbank her
+                MySqlDataAdapter mda = new MySqlDataAdapter(
+                "select * from auto.auto into outfile 'C://Users//Ramona//Desktop//Praktikum//Mysql2.csv'"
+                + "Fields terminated By',' Lines Terminated By'\n' ", mcon);
+                //bestimmte Tabelle wird in eine CSV-Datei konvertiert
+                mcon.Open();
+                DataSet ds = new DataSet();
+                mda.Fill(ds, "auto"); //CSV-Datei wird mit den Werten aus "auto" gefüllt
+                mcon.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void button3_Click(object sender, RibbonControlEventArgs e)
+        {
+            int zeile = 1;
+            string[] separator = { ",", "\r\n", };
+            var ersteZeile = File.ReadLines("C:\\Users\\Ramona\\Desktop\\Praktikum\\mysql2.csv").First();
+            //var --> definition einer nicht typisierten Variable (konkreter Typ ergibt sich aus dem zugewiesenen Wert)
+            int zähler = ersteZeile.Split(separator, StringSplitOptions.None).Length;
+            string spalte = Char.ConvertFromUtf32(zähler + 64);
+            var reader = new StreamReader(File.OpenRead("C:\\Users\\Ramona\\Desktop\\Praktikum\\mysql2.csv"));
+            List<string[]> values = new List<string[]>();
+
+            Excel.Window window = e.Control.Context;  //aktives Fenster wird dargestellt
+            Excel.Worksheet activeWorksheet = ((Excel.Worksheet)window.Application.ActiveSheet);
+            //gibt ein Objekt zurück, welches das aktuelle Blatt in der aktiven Arbeitsmappe darstellt 
+            Excel.Range newWorksheet = activeWorksheet.get_Range("A" + zeile, spalte + zeile);
+            //stellt eine Zelle/ein Zellenbereich dar 
+            while (!reader.EndOfStream)
+            {
+                string line = reader.ReadLine();
+                values.Add(line.Split(separator, StringSplitOptions.None)); //erste Zeile wird gesplittet hinzugefügt
+                newWorksheet.Value2 = line.Split(separator, StringSplitOptions.None);
+                zeile++;
+                while (!reader.EndOfStream)
+                {
+                    Excel.Range newWorksheet2 = activeWorksheet.get_Range("A" + zeile, spalte + zeile);
+                    string newline = reader.ReadLine();
+                    values.Add(newline.Split(separator, StringSplitOptions.None));
+                    newWorksheet2.Value2 = newline.Split(separator, StringSplitOptions.None);
+                    zeile++;
+                }
+            }
+        }
+
+        private void button4_Click(object sender, RibbonControlEventArgs e)
+        {
+            string connectionString = "datasource=localhost;port=3306;username=root;password=";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            string MySql = "SELECT * FROM auto.auto";
+            MySqlCommand cmd = new MySqlCommand(MySql, connection);
+            MySqlDataReader reader;
+
+            try
+            {
+                connection.Open();
+                reader = cmd.ExecuteReader(); //es wird eine MySqlDataReader-Klasse erstellt
+                while (reader.Read())
+                {
+                    MessageBox.Show(reader.GetValue(0) + " , " + reader.GetValue(1) + " , "
+                    + reader.GetValue(2) + " , " + reader.GetValue(3));
+                }
+                reader.Close();
+                cmd.Dispose();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Can not open connection ! ");
+            }
+        }
+
+        private void button5_Click(object sender, RibbonControlEventArgs e)
+        {
+            int zeile = 1;
+            string[] separator = { ",", "\r\n" };
+            string connectionString = "datasource=localhost;port=3306;username=root;password=";
+            MySqlConnection connection = new MySqlConnection(connectionString);
+            MySqlCommand command = connection.CreateCommand();
+            command.CommandText = "SELECT * FROM auto.auto;";
+            //command.CommandText = "SELECT * FROM test_excel.test_excel";
+            MySqlDataReader reader;
+            Excel.Window window = e.Control.Context; //aktives Fenster wird dargestellt
+            Excel.Worksheet activeWorksheet = ((Excel.Worksheet)window.Application.ActiveSheet);
+            //gibt ein Objekt zurück, das das aktuelle Blatt in der aktiven Arbeitsmappe darstellt 
+            string commandLine =
+            "SELECT COUNT(*) FROM information_schema.COLUMNS WHERE table_schema = 'auto' and table_name = 'auto'";
+            //string commandLine = 
+            //"SELECT COUNT(*) FROM information_schema.COLUMNS WHERE table_schema ='test_excel' and table_name='test_excel'";
+            using (MySqlConnection connect = new MySqlConnection(connectionString))
+            using (MySqlCommand cmd = new MySqlCommand(commandLine, connect))
+            {
+                connect.Open(); //Verbindung wird geöffnet
+                try
+                {
+                    int i;
+                    int zähler = Convert.ToInt32(cmd.ExecuteScalar()); //führt abfragen aus & gibt erste zeile&spalte zurück
+                    string spalte = Char.ConvertFromUtf32(zähler + 64);
+                    Excel.Range newWorksheet = activeWorksheet.get_Range("A" + zeile, spalte + zeile);
+                    //stellt eine Zelle/ein Zellenbereich dar
+                    connection.Open();
+                    reader = command.ExecuteReader(); //es wird eine MySqlDataReader-Klasse erstellt
+                    while (reader.Read())
+                    {
+                        string inhalt = "";
+                        for (i = 0; i < reader.FieldCount; i++)//ruft die Anzahl der Spalten in der aktuelle Zeile ab
+                        {
+                            inhalt += reader.GetValue(i).ToString() + " , "; //weist links den Wert von links+rechts zu 
+                        }
+                        newWorksheet.Value2 = inhalt.Split(separator, StringSplitOptions.None);
+                        zeile++;
+                        while (reader.Read())
+                        {
+                            string inhalt2 = "";
+                            Excel.Range newWorksheet2 = activeWorksheet.get_Range("A" + zeile, spalte + zeile);
+                            for (i = 0; i < reader.FieldCount; i++)
+                            {
+                                inhalt2 += reader.GetValue(i).ToString() + " , ";
+                            }
+                            newWorksheet2.Value2 = inhalt2.Split(separator, StringSplitOptions.None);
+                            zeile++;
+                        }
+                    }
+                    reader.Close();
+                    command.Dispose();
+                    connection.Close();
+                    connect.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Can not open connection ! ");
+                }
+            }
+        }
+
+    }
+}
